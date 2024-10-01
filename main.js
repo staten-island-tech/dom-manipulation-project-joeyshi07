90d1194943afb33dbe2d2c7fbc35dbaae11ba147
joeyshi07 <joeys19@nycstudents.net>
2024-10-01 13:58:35 -0400

    edited html and js

---
+++ main.js
@@ -0,0 +1,18 @@
+const DOMSelectors = {
+  header: document.querySelector("h1"),
+  button: document.querySelector("."),
+  form: document.querySelector(".form"),
+};
+
+DOMSelectors.button.addEventListener("click", function (event) {
+  console.log(event.target.parentElement);
+  event.target.style.backgroundColor = "red";
+});
+
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault(); // not refreshing page
+  console.log(document.querySelector("input").value);
+});
+
+console.log(DOMSelectors.header);
+console.log(DOMSelectors.items);
