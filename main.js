440b63a7127d08a76127ef86e647770fb3f4b505
joeyshi07 <joeys19@nycstudents.net>
2024-10-11 13:58:09 -0400

    progress on proj

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const DOMSelectors = {
   button: document.querySelector("#button"),
   form: document.querySelector(".form"),
   image: document.querySelector(".album-cover"),
+  container: document.querySelector(".container"),
 };
 
 DOMSelectors.button.addEventListener("click", function (event) {
@@ -12,13 +13,19 @@ DOMSelectors.button.addEventListener("click", function (event) {
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault(); // not refreshing page
-  console.log(document.querySelector("input").value);
-});
+  const albumName = document.querySelector(".album-name").value;
+  const artistName = document.querySelector(".artist-name").value;
+  const albumCover = document.querySelector(".album-cover").value;
 
-DOMSelectors.container.insertAdjacentHTML(
-  "beforeend",
-  `<div class = "card">< class = "card-header"> ${} </h2></div>`
-);
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `<div class = "card">
+  <h2 class = "card-header"> ${albumName}</h2>
+  <img src="${albumCover}" alt="${albumName} cover" class = "album-cover" />
+  <p id = "artist-name"> By: ${artistName} </p>
+  </div>`
+  );
+});
 
 //create HTML for inputs, cards, container
 // select/query the HTML form and get valyes from inputs
