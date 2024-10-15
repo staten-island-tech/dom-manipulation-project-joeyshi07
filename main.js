595360042dd2ef3a6b6276effc0c4e5d8e68c115
joeyshi07 <joeys19@nycstudents.net>
2024-10-14 21:34:02 -0400

    delete feature + css

--- main.js
+++ main.js
@@ -2,29 +2,38 @@ const DOMSelectors = {
   header: document.querySelector("h1"),
   button: document.querySelector("#button"),
   form: document.querySelector(".form"),
+  food: document.querySelector(".food-name"),
+  course: document.querySelector(".course-name"),
   image: document.querySelector(".jellycat-pic"),
   container: document.querySelector(".container"),
 };
 
-DOMSelectors.button.addEventListener("click", function (event) {
-  console.log(event.target.parentElement);
-  event.target.style.backgroundColor = "red";
-});
-
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault(); // not refreshing page
-  const foodName = document.querySelector(".food-name").value;
-  const courseName = document.querySelector(".course-name").value;
+  const foodName = DOMSelectors.food.value;
+  const courseName = DOMSelectors.course.value;
   const jellycatPicture = DOMSelectors.image.value;
 
   DOMSelectors.container.insertAdjacentHTML(
     "beforeend",
     `<div class = "card">
-  <h2 class = "card-header"> ${foodName}</h2>
+  <h3 class = "card-header"> ${foodName}</h3>
   <img src="${jellycatPicture}" alt="${foodName} cover" class = "jellycat" />
   <p id = "course-name"> ${courseName} </p>
+  <button class = "delete-button"> x </button> 
   </div>`
   );
+
+  DOMSelectors.food.value = "";
+  DOMSelectors.course.value = "";
+  DOMSelectors.image.value = "";
+
+  const deleteButtons = document.querySelectorAll(".delete-button");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      button.parentElement.remove();
+    });
+  });
 });
 
 //create HTML for inputs, cards, container
